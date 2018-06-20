129faf95cf8428ea49fb97cbc3c3a2271ec681e8
luccasMelo <luccas.melo97@gmail.com>
2018-06-20 14:31:38 -0300

    Generic adapter update

--- kotlinutils/src/main/java/com/luccasmelo/kotlinutils/GenericAdapter.kt
+++ kotlinutils/src/main/java/com/luccasmelo/kotlinutils/GenericAdapter.kt
@@ -2,42 +2,112 @@ package com.luccasmelo.kotlinutils
 
 import android.databinding.DataBindingUtil
 import android.databinding.ViewDataBinding
+import android.support.annotation.MainThread
+import android.support.v7.util.DiffUtil
 import android.support.v7.widget.RecyclerView
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import org.jetbrains.annotations.Nullable
 
-abstract class GenericAdapter<T> : RecyclerView.Adapter<GenericViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup,
-                                    viewType: Int): GenericViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val binding = DataBindingUtil.inflate<ViewDataBinding>(
-                layoutInflater, viewType, parent, false)
-        return GenericViewHolder(binding)
+
+abstract class GenericAdapter<ITEM> constructor(protected var itemList: ArrayList<ITEM>,
+                                                 private val layoutResId: Int, private val bindId: Int) : RecyclerView.Adapter<GenericAdapter.GenericViewHolder>() {
+
+
+    override fun getItemCount() = itemList.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): GenericViewHolder {
+
+        val view = parent inflate layoutResId
+        val viewHolder = GenericViewHolder(view)
+        val itemView = viewHolder.itemView
+        itemView.setOnClickListener {
+            val adapterPosition = viewHolder.adapterPosition
+            if (adapterPosition != RecyclerView.NO_POSITION) {
+                onItemClick(itemView, adapterPosition)
+            }
+        }
+        return viewHolder
     }
 
-    override fun onBindViewHolder(holder: GenericViewHolder,
-                                  position: Int) {
-        val obj = getObjForPosition(position)
-        holder.bind(obj, getVarId())
+    override fun onBindViewHolder(holder: GenericAdapter.GenericViewHolder, position: Int) {
+        val item = itemList[position]
+        holder.bind(item!!, bindId)
     }
 
-    override fun getItemViewType(position: Int): Int {
-        return getLayoutIdForPosition(position)
+
+    @MainThread
+    fun update(items: ArrayList<ITEM>) {
+        val diffResult = DiffUtil.calculateDiff(DiffUtilCallback(itemList, items), false)
+        this.itemList = items
+        diffResult.dispatchUpdatesTo(this)
     }
 
-    protected abstract fun getVarId():Int
-    protected abstract fun getObjForPosition(position: Int): Any
+    private fun updateAdapterWithDiffResult(result: DiffUtil.DiffResult) {
+        result.dispatchUpdatesTo(this)
+    }
 
-    protected abstract fun getLayoutIdForPosition(position: Int): Int
+    private fun calculateDiff(newItems: MutableList<ITEM>) =
+            DiffUtil.calculateDiff(DiffUtilCallback(itemList, newItems))
 
-    protected abstract fun update(objects: List<T>)
+    fun add(item: ITEM) {
+        itemList.add(item)
+        notifyItemInserted(itemList.size)
+    }
+
+    fun remove(position: Int) {
+        itemList.removeAt(position)
+        notifyItemRemoved(position)
+    }
+
+
+    protected open fun onViewRecycled(itemView: View) {
+    }
+
+    protected open fun onItemClick(itemView: View, position: Int) {
+    }
+
+
+    override fun onViewRecycled(holder: GenericViewHolder) {
+        super.onViewRecycled(holder)
+        this.onViewRecycled(holder.itemView)
+
+    }
+
+    class GenericViewHolder internal constructor(private val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(any: Any, id: Int) {
+            binding.setVariable(id, any)
+            binding.executePendingBindings()
+        }
+    }
+
+
 }
 
-class GenericViewHolder internal constructor(private val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root) {
+internal class DiffUtilCallback<ITEM>(private val oldItems: MutableList<ITEM>,
+                                      private val newItems: MutableList<ITEM>) : DiffUtil.Callback() {
 
-    fun bind(any: Any, id:Int) {
+    override fun getOldListSize() = oldItems.size
 
-       binding.setVariable(id, any)
-        binding.executePendingBindings()
+    override fun getNewListSize() = newItems.size
+
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+            oldItems[oldItemPosition] == newItems[newItemPosition]
+
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+            oldItems[oldItemPosition] == newItems[newItemPosition]
+
+    @Nullable
+    override fun getChangePayload(oldItemPosition: Int, newItemPosition: Int): Any? {
+
+        return super.getChangePayload(oldItemPosition, newItemPosition)
     }
 }
+
+infix fun ViewGroup.inflate(layoutResId: Int): ViewDataBinding =
+        DataBindingUtil.inflate<ViewDataBinding>(
+                LayoutInflater.from(this.context), layoutResId, this, false)
+
+
